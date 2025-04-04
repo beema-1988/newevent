d7460993bc31a9489eb977eba4ff7f01a76256ad
beema-1988 <you@example.com>
2025-04-04 20:10:27 +0400

    complete updated code -4

--- client/src/axios/axiosInstance.js
+++ client/src/axios/axiosInstance.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 
-const url = import.meta.env.VITE_BASE_URL 
-console.log(url,"baseURL");
+// const url = import.meta.env.VITE_BASE_URL 
+// console.log(url,"baseURL");
+
+// const axiosInstance=axios.create({
+//     baseURL:url,
+//     withCredentials:true
+// })
+
+const API_BASE_URL = import.meta.env.VITE_BASE_URL 
+console.log(API_BASE_URL,"baseURL");
 
 const axiosInstance=axios.create({
-    baseURL:url,
+    baseURL:API_BASE_URL,
     withCredentials:true
 })
 
