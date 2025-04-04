e8da8eeed2a04c17d1b8460073c04cc7249deb1a
beema-1988 <you@example.com>
2025-04-04 19:59:41 +0400

    complete updated code -2

--- server/src/index.js
+++ server/src/index.js
@@ -28,7 +28,7 @@ app.use(cookieparser())
 // }));
 
  app.use(cors({
-    origin: ["https://neweventfront.vercel.app", "http://localhost:3000"], 
+    origin: ["https://neweventfront.vercel.app", "http://localhost:5173"], 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
      allowedHeaders: ["Content-Type", "Authorization"]
